a536ca7317d3d2968cc9fa48367898725d0d3bc8
Superfluxus <38507824+Superfluxus@users.noreply.github.com>
2019-03-06 12:10:27 +0000

    Add files via upload

---
+++ Discover GPOs.ps1
@@ -0,0 +1,49 @@
+﻿
+Function Dump-GPOs {
+    
+    Param (
+        [Parameter(Mandatory=$true)][String]$OutputDirectory
+    )
+
+    #Remove trailing slash if present.
+    If ($OutputDirectory -like "*\"){$OutputDirectory = $OutputDirectory.substring(0,($OutputDirectory.Length-1))}
+
+    $GPOs = get-gpo -All
+    $AllGPOs = @()
+
+    If (!(Test-Path $OutputDirectory)){mkdir $OutputDirectory -Force | Out-Null}
+
+    ForEach ($GPO in $GPOs) {
+        $GPO.DisplayName = $GPO.DisplayName.Replace('/','')
+        $AllGPOs += $GPO.DisplayName
+        Write-Output ("Exporting " + $OutputDirectory + "\" + $GPO.DisplayName + ".HTML...")
+        $Path = $OutputDirectory + "\" + $GPO.DisplayName + ".HTML"
+        Get-GPOReport -Name $GPO.DisplayName -ReportType HTML -Path $Path
+        }
+    $AllGPOs = $AllGPOs | Sort-Object
+    Write-Output ("Exporting " + $OutputDirectory + "\AllGPOs.txt...")
+    $AllGPOs | Out-File ($OutputDirectory + "\AllGPOs.txt")
+
+    $OUs = Get-ADOrganizationalUnit -Filter * | Sort-Object {-join ($_.distinguishedname[($_.distinguishedname.length-1)..0])}
+    $OutputArray = @()
+    $OUs | ForEach-Object {
+        $Inheritance = Get-GPInheritance -Target $_.DistinguishedName
+
+        $GpoLinks = @()
+        If ($Inheritance.GpoLinks.DisplayName){
+            ForEach ($i in 0..($Inheritance.GpoLinks.DisplayName.Count -1)){
+                $GpoLinks += $Inheritance.GpoLinks[$i].Order.toString() + ": " + $Inheritance.GpoLinks[$i].DisplayName
+            }
+        }
+
+        $Obj = New-Object -TypeName PSObject
+        $Obj | Add-Member -MemberType NoteProperty -Name "Path" -Value $Inheritance.Path
+        $Obj | Add-Member -MemberType NoteProperty -Name "GpoLinks" -Value ($GpoLinks -join ", ")
+        $OutputArray += $Obj
+    }
+
+    Write-Output ("Exporting " + $OutputDirectory + "\GPOLinks.csv...")
+    $OutputArray | Export-CSV ($OutputDirectory+"\GPOLinks.csv") -NoTypeInformation
+}
+
+Dump-GPOs -OutputDirectory "C:\temp"
